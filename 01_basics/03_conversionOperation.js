c3677e9b0afe96aa513dd698b2d8a8caffe36fc6
Atul Dwivedi <atultechd@gmail.com>
2023-09-21 08:20:48 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let age = "22a"
+
+// console.log(typeof age)
+
+let new_age = Number(age)
+
+// console.log(new_age)
+
+/*
+"22" = 22 after conversion to num
+"22a" = NaN after conversion because it 
+true = 1 and false = 0
+*/
+
+let isLoggedIn = 1;
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(boolIsLoggedIn)
+
+/*
+1 = true and 0 = false
+"" = false
+"atul" = true
+*/
