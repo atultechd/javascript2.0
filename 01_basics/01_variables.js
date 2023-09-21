f5dda1e0e2559892e5586813a70cf81b7d253602
Atul Dwivedi <atultechd@gmail.com>
2023-09-21 07:59:20 +0000

    started datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 77
+let accountEmail = "atultechd@gmail.com"
+var accountPassword = "123"
+
+accountCity = "Delhi"
+
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+let name = "atul"
+
+let age = 22
+
+/*
+number = 2 to power 53
+bigInt
+string = ""
+boolean = true/false
+null = standalone value
+undefined = 
+symbol = unique
+*/
+
+// object
+
+console.log(typeof age);
+
+console.log(typeof undefined)
+
+console.log(typeof null)
